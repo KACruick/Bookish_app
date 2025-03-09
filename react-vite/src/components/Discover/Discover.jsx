31dab9987f087becf074b0adfc1e8f935d5ad714
Kendra <cruickshank.kendra@gmail.com>
2025-03-09 15:43:18 -0700

    started discover - shows all books

--- react-vite/src/components/Discover/Discover.jsx
+++ react-vite/src/components/Discover/Discover.jsx
@@ -16,10 +16,9 @@ function Discover() {
     const fetchBooks = async () => {
       setLoading(true);
       try {
-        const response = await axios.get('/api/books', {
-          params: { search: searchQuery, page: 1, size: 20 }, // Pass search query to the backend
-        });
-        setBooks(response.data.books);
+        const response = await fetch(`/api/books?search=${searchQuery}&page=1&size=20`);
+        const data = await response.json();
+        setBooks(data.books);
       } catch (error) {
         setError('Failed to fetch books');
       } finally {
@@ -30,30 +29,31 @@ function Discover() {
     if (searchQuery) {
       fetchBooks();
     } else {
-      // If there's no search query, you can fetch all books
       fetchBooks();
     }
-  }, [searchQuery]); // This will run when searchQuery changes
+  }, [searchQuery]); 
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>{error}</p>;
 
   return (
-    <div>
-      <h1>Discover Books</h1>
+    <div className='discover-page'>
+      {/* <h1>Discover Books</h1> */}
       {books.length === 0 ? (
         <p>No books found for "{searchQuery}"</p>
       ) : (
-        <div className="books-list">
+        <div className="discover-books-grid">
           {books.map((book) => (
-            <div key={book.id} className="book-tile">
-              <img src={book.coverPicture || '/default-book-cover.png'} alt={book.title} />
-              <div className="book-info">
+            <div key={book.id} className="discover-book-tile">
+              <img src={book.coverPicture || '/default-book-cover.png'} alt={book.title} className="discover-book-cover"/>
+              
+              <div className="discover-book-info">
                 <h3>{book.title}</h3>
                 <p>{book.author}</p>
-                <p>{book.genreId}</p>
+                {/* <p>{book.genreId}</p> */}
                 <p>{book.avgRating} / 5</p>
               </div>
+
             </div>
           ))}
         </div>
